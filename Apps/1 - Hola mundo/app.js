a71e9ba519e7e713f33064bb337e119b40ad1751
Benjamín Ayala <bayala0@ucol.mx>
2022-09-29 00:38:52 -0500

    Seccion 1: Introduccion a TypeScript y Hola Mundo

---
+++ Apps/1 - Hola mundo/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const saludo = "Hola Mundo!";
+const heroe = {
+    nombre: "Ironman",
+    edad: 45,
+};
+heroe.edad = heroe.edad + 1;
+console.log(saludo);
+console.log(heroe.nombre);
+console.log(heroe.edad);
---
+++ Apps/1 - Hola mundo/app.ts
@@ -0,0 +1,12 @@
+const saludo:String = "Hola Mundo!";
+
+const heroe = {
+	nombre: "Ironman",
+	edad: 45,
+}
+
+heroe.edad = heroe.edad + 1;
+
+console.log(saludo)
+console.log(heroe.nombre)
+console.log(heroe.edad)
